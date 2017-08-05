5273aeb6b9c2e9b478117609635d318b74b29e80
Alexandr <zigfridone@gmail.com>
2017-08-05 14:24:22 +0400

    новое начало
    сделана первоначальная оболочка

--- necrom_main.pas
+++ necrom_main.pas
@@ -24,12 +24,18 @@
 
 program necrom_main;
 
-uses crt;
-var i : byte;
+uses sysutils{,linux},crt,windows,dateutils,Zipper;
+var 
+i : word;
+menu_key:char;
+lang,lang_f: text;
+text:array[0..200] of string[100];
+lang_s:string[5];
+log:string[10];
 procedure main_menu;
-begin
+begin//0
 
-repeat begin//1
+repeat //begin//1
 ClrScr;
 writeln	('1- ',text[10]);
 writeln	('2- ',text[9]);
@@ -43,13 +49,36 @@ case menu_key of
 
 '3': begin//1.4
 end;//1.4
-end;
-end;//2 
+
+end;//1
+
 until menu_key='0';
 
-end;
+end;//0
 BEGIN
-	
+i:=0;
+assign(lang_f,'lang_f');
+reset(lang_f);
+readln(lang_f,lang_s);
+readln(lang_f,log);
+close(lang_f);
+//if log='full' then log_generate('log_old_generate','lang = '+lang_s);
+//if (lang_s<>'w_rus')or (lang_s<>'u_rus')or (lang_s<>'w_eng')or (lang_s<>'u_eng') then begin writeln	('error file lang_f'); exit; end;
+//if log='full' then log_generate('log_old_generate','close lang_f');
+//if log='full' then log_generate('log_old_generate','start lang'+' '+lang_s);
+if lang_s='w_rus' then assign(lang,'res\lang\rus\text_win.lang');
+if lang_s='w_eng' then assign(lang,'res\lang\eng\text_win.lang');
+if lang_s='u_eng' then assign(lang,'res/lang/eng/text_unix.lang');
+if lang_s='u_rus' then assign(lang,'res/lang/rus/text_unix.lang');
+//if log='full' then log_generate('log_old_generate','off lang'+' '+lang_s);
+reset(lang);
+while not eof(lang) do begin
+readln(lang,text[i]);
+delete(text[i],1,3);
+i:=i+1;
+end;
+close(lang);
+main_menu;
 	
 END.
 
